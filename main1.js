078328951e098cd7ddac7a62d922848f6a79c8e4
bsravya1385 <53679956+bsravya1385@users.noreply.github.com>
2019-08-04 11:27:53 -0700

    Update main1.js

--- main1.js
+++ main1.js
@@ -2,7 +2,7 @@ async function init() {
 const margin = 50;
 const width = 800 - 2 * margin;
 const height = 800 - 2 * margin;
-const data = await d3.csv('https://flunky.github.io/cars2017.csv');
+const data = await d3.csv('World_Indicators.csv');
 const svg = d3.select("svg")
   .attr("width", 800)
    .attr("height", 800);
@@ -25,9 +25,9 @@ chart.selectAll()
     .data(data)
     .enter()
     .append("circle")
-  .attr("cx", function(d, i) {return xscale10(d.AverageCityMPG);})
-  .attr("cy", function(d, i) { return yscale10(d.AverageHighwayMPG);})
-  .attr("r", function(d) {return (parseInt(d.EngineCylinders)+2);});
+  .attr("cx", function(d, i) {return xscale10(d.Infant Mortality Rate);})
+  .attr("cy", function(d, i) { return yscale10(d.Birth Rate);})
+  .attr("r", function(d) {return (2);});
 
 chart.append('g')
     .attr("transform", "translate(" + margin + "," + margin + ")")
